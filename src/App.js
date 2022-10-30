b81c4bc0a135a94358a401fce38b3e718a57074c
gabrieln96 <gabrielaraujo.n96@gmail.com>
2022-10-30 17:36:09 -0300

    estrutura pt 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import './App.css';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Home from './components/pages/Home';
+import Contato from './components/pages/Contato';
+import NovoProjeto from './components/pages/NovoProjeto';
+import Sobre from './components/pages/Sobre';
+
+import Container from './components/layout/Container';
+
 
 function App() {
   return (
-    <div className="App">
-     <p>GabsT</p>
-    </div>
-  );
+    <Router>
+      <ul>
+        <Link to="/">Home</Link>
+        <Link to="/sobre">Sobre</Link>
+        <Link to="/contato">Contato</Link>
+        <Link to="/novoprojeto">Novo Projeto</Link>
+      </ul>
+      <Container customClass="min-height">
+       <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/sobre" element={<Sobre/>}/>
+        <Route path="/contato" element={<Contato/>}/>
+        <Route path="/novoprojeto" element={<NovoProjeto/>}/>   
+        </Routes>
+        </Container>
+      <p>Footer</p>
+  
+    </Router>
+    
+  )
 }
 
 export default App;
