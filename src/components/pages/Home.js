cbce3c5e891ae73173f7ee241e2d1539593c4219
gabrieln96 <gabrielaraujo.n96@gmail.com>
2022-10-31 19:40:33 -0300

    home

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,15 @@
+import styles from './Home.module.css'
+import savings from '../../img/saving.svg'
+import LinkButton from '../layout/LinkButton'
+
 function Home (){
     return(
-        <h1>Home</h1>
+        <section className={styles.home_container}>
+            <h1>Bem-vindo ao <span>GabsT</span></h1>
+            <p>Gerencie seus projetos agora mesmo!</p>
+            <LinkButton to="/novoprojeto" text="Criar Projeto"/>
+            <img src={savings} alt="GabsT"/>
+        </section>
     )
 }
 
